183113adad7587e0e8a02c9825e1d6c404073cdd
juantoranzos <toranzos628@gmail.com>
2023-04-04 17:45:50 -0300

    funcion sacar tarea

--- js/app.js
+++ js/app.js
@@ -1,17 +1,17 @@
 
 function añadirLista() {
-    let lista = document.getElementById("newTask").value;
+    let lista = document.getElementById("nuevaTarea").value;
     if (lista) {
-        let taskList = document.getElementById("taskList");
-        let newTask = document.createElement("li");
-        newTask.className = "list-group-item";
-        newTask.innerHTML = lista + ' <button class="btn btn-danger btn-xs" onclick="removeTask(this)">Eliminar</button>';
-        taskList.appendChild(newTask);
-        document.getElementById("newTask").value = "";
+        let listaDeTareas = document.getElementById("listaDeTareas");
+        let nuevaTarea = document.createElement("li");
+        nuevaTarea.className = "list-group-item";
+        nuevaTarea.innerHTML = lista + ' <button class="btn btn-danger btn-xs" onclick="sacarTarea(this)">Eliminar</button>';
+        listaDeTareas.appendChild(nuevaTarea);
+        document.getElementById("nuevaTarea").value = "";
     }
 }
 
-function removeTask(button) {
+function sacarTarea(button) {
     let lista = button.parentNode;
     lista.parentNode.removeChild(lista);
 }
